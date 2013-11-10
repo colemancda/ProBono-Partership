68bef4ccf01d49a1233005adb2f43679ed5e71e9
Alsey Coleman Miller <alseycmiller@gmail.com>
2013-11-09 23:58:51 -0500

    added retina image

--- ProBono Partership/PBPAppDelegate.m
+++ ProBono Partership/PBPAppDelegate.m
@@ -12,7 +12,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
     
