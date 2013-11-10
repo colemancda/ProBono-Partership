98f87b427e571477f4486116c5ef763fcbd2567b
Alsey Coleman Miller <alseycmiller@gmail.com>
2013-11-10 10:49:46 -0500

    working on API

---
+++ ProBono Partership/PBPAPI.h
@@ -0,0 +1,17 @@
+//
+//  PBPAPI.h
+//  ProBono Partership
+//
+//  Created by Alsey Coleman Miller on 11/10/13.
+//  Copyright (c) 2013 CDA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PBPAPI : NSObject
+
+@property NSURLSession *session;
+
+-(void)fetchCategories:()
+
+@end
---
+++ ProBono Partership/PBPAPI.m
@@ -0,0 +1,13 @@
+//
+//  PBPAPI.m
+//  ProBono Partership
+//
+//  Created by Alsey Coleman Miller on 11/10/13.
+//  Copyright (c) 2013 CDA. All rights reserved.
+//
+
+#import "PBPAPI.h"
+
+@implementation PBPAPI
+
+@end
